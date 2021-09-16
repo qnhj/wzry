6f6ec54f9a0ce8bd964e77ff85145530af17f991
qnhj <2338171753@qq.com>
2021-09-16 18:32:48 +0800

    实现了爬取所有海报

--- src/baochao/xyz/Main.java
+++ src/baochao/xyz/Main.java
@@ -4,28 +4,29 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        String path = "D:/wzry";
 
 
-        String path = "E:/wzry";
         File file = new File(path);
-        if (!file.exists()){
+        if (!file.exists()) {
             file.mkdirs();
         }
 
         //获取英雄和数字对应关系
-        HashMap<String,String> hashMap = NumAndHero.numAndHero();
+        HashMap<String, String> hashMap = NumAndHero.numAndHero();
 
         Iterator i = hashMap.entrySet().iterator();
-        while (i.hasNext()){
-            Map.Entry entry = (Map.Entry)i.next();
+        while (i.hasNext()) {
+            Map.Entry entry = (Map.Entry) i.next();
             //下载图片
-            Downloads.begin(path,entry);
+            Downloads.begin(path, entry);
         }
 
-        System.out.println("\n图片已保存在"+path);
+        System.out.println("\n图片已保存在" + path);
     }
 }
